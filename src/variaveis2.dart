b90f458c241b277d4ae3ba7c8a55d5a30f058a8e
Danilo Sacramento <89158678+DaniloSacramento@users.noreply.github.com>
2022-09-06 14:37:14 -0300

    voltando a estudar dart

---
+++ src/variaveis2.dart
@@ -0,0 +1,9 @@
+main(){
+  var a = 2;
+  var b = 32;
+  print(a + b);
+  var texto = " Danilo";
+  print(texto);
+  print(texto  + ( a + b).toString());
+  print("ola mundo");
+}
---
+++ src/variaveis_1.dart
@@ -0,0 +1,7 @@
+main(){
+  int soma = 2 + 2;
+  double b = 3.2 + 3.18;
+  print(b);
+  print(soma);
+  print(b * soma);
+}
