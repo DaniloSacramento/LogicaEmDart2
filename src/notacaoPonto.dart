b6bfaca47f794caa3aa25842d73efcd7cb046233
Danilo Sacramento <89158678+DaniloSacramento@users.noreply.github.com>
2022-09-08 19:45:32 -0300

    notaçãoPonto

---
+++ src/notacaoPonto.dart
@@ -0,0 +1,14 @@
+main(){
+  double nota = 6.4.roundToDouble();
+  print(nota);
+
+  String texto = "texto".toUpperCase();
+  print(texto);
+  String s1 = "Danilo Sacramento";
+  String s2 = s1.substring(0,2);
+  String s3 = s2.toUpperCase();
+  String s4 = s3.padRight(17, "!!!!!!");
+
+  print(s4);
+
+}
