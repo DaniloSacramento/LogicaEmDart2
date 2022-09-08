008bed32037af308d7abcba3f241cc515f2ce057
Danilo Sacramento <89158678+DaniloSacramento@users.noreply.github.com>
2022-09-08 18:27:56 -0300

    Array, MAP, SET

--- src/tiposBasicos.dart
+++ src/tiposBasicos.dart
@@ -6,37 +6,33 @@
 */
 
 import 'dart:io';
-main(){
-int n1 = 4;
-double n2 = (-5.68).abs();
-double n3 = double.parse("15.88");
-print(n1 + n2 + n3 );
 
+main() {
+  int n1 = 4;
+  double n2 = (-5.68).abs();
+  double n3 = double.parse("15.88");
+  print(n1 + n2 + n3);
 
+  String texto = "Danilo";
+  String texto2 = "Sacramento";
 
-String texto = "Danilo";
-String texto2 = "Sacramento";
+  print(texto + texto2);
 
-print(texto + texto2);
+  dynamic x = "ola";
+  print(x);
 
-dynamic x = "ola";
-print(x);
+  x = 123;
+  print(x);
 
- x = 123;
-print(x);
-
-
-bool condicao = true;
-while(condicao){
-  print("Está frio hoje?");
-  String tempo = stdin.readLineSync()!;
-  if(tempo == "sim"){
-    condicao = false;
-    print("o tempo está frio");
-
-  }else if(tempo == "não"){
-    print("o tempo não está frio");
-   
-}
-}
+  bool condicao = true;
+  while (condicao) {
+    print("Está frio hoje?");
+    String tempo = stdin.readLineSync()!;
+    if (tempo == "sim") {
+      condicao = false;
+      print("o tempo está frio");
+    } else if (tempo == "não") {
+      print("o tempo não está frio");
+    }
+  }
 }
--- src/tiposBasicos2.dart
+++ src/tiposBasicos2.dart
@@ -4,7 +4,19 @@
   */
 
 main() {
-  List aprovados = ["Ana", "Calos", "Danilo", "Sacramento"];
+  // LIST
+  List<String> aprovados = ["Ana", "Calos", "Danilo", "Sacramento"]; // 0, 1, 2, 3... 
   print(aprovados.length);
-  print(aprovados.elementAt(2));
+  print(aprovados.elementAt(2)); 
+  print(aprovados[0]);
+
+  // MAP
+  Map<String, String> telefone = {
+    "Joao" : "999977889",
+    "Danilo": "99881234",
+    "Ana" : "9977665522"
+  };
+print(telefone);
+
 }
+ 
