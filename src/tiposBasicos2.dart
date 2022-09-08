f1dfa56f276f66838a3bf40418a3b88f66eac5bc
Danilo Sacramento <89158678+DaniloSacramento@users.noreply.github.com>
2022-09-08 18:48:27 -0300

    set, array, map2

--- src/tiposBasicos2.dart
+++ src/tiposBasicos2.dart
@@ -5,18 +5,35 @@
 
 main() {
   // LIST
-  List<String> aprovados = ["Ana", "Calos", "Danilo", "Sacramento"]; // 0, 1, 2, 3... 
+  List<String> aprovados = [
+    "Ana",
+    "Calos",
+    "Danilo",
+    "Sacramento"
+  ]; // 0, 1, 2, 3...
   print(aprovados.length);
-  print(aprovados.elementAt(2)); 
+  print(aprovados.elementAt(2));
   print(aprovados[0]);
 
   // MAP
   Map<String, String> telefone = {
-    "Joao" : "999977889",
+    "Joao": "999977889",
     "Danilo": "99881234",
-    "Ana" : "9977665522"
+    "Ana": "9977665522"
   };
-print(telefone);
+  print(telefone);
+  print(telefone["Danilo"]);
+  print(telefone.length);
+  print(telefone.keys);
+  print(telefone.values); 
 
+  // SET
+
+  Set times = {"nautico", "csa", "ceara","Santos"};
+  print(times.add("SportNanico"));
+  print(times);
+  print(times.last);
+  print(times.first);
+ aprovados.add("Danilo");
+  print(aprovados.length);
 }
- 
