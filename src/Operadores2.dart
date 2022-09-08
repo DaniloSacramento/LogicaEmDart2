4bbc78fd19374dc277abdf846d895d5b82152d67
Danilo Sacramento <89158678+DaniloSacramento@users.noreply.github.com>
2022-09-08 20:32:14 -0300

    Operadores 2

---
+++ src/Operadores2.dart
@@ -0,0 +1,16 @@
+main(){
+  // Operadores de Atribuição 
+  int A = 2;
+  A = A + 3;
+  A += 3;
+  print(A);
+
+  // Operadores Relacionais, sempre da um resultado BOLL;
+
+  print(3>2);
+  print(3>=2);
+  print(3<2);
+  print(3<=2);
+  print(3!=2);
+  print(2 + 8 > 10 - 1);
+}
