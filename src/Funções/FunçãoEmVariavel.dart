9f1c5d51f3ad03f567faa04abec93aa4006b200d
Danilo Sacramento <89158678+DaniloSacramento@users.noreply.github.com>
2022-09-14 14:34:02 -0300

    função anonimas

---
+++ src/Funções/FunçãoEmVariavel.dart
@@ -0,0 +1,16 @@
+main(){
+  
+  // TIPO NOME = VALOR;
+  int Function (int,int)soma1 = somafn;  // INT = TIPO, SOMA1 = NOME, SOMAFN = VALOR
+  print(soma1(20, 313)); 
+
+  int Function(int, int)soma2 = (x, y){
+   return x + y;
+
+  };
+  print(soma2(20, 313));
+}
+
+int somafn(int a, int b){
+  return a+b;
+}
---
+++ src/Funções/FunçãoEmVariavel2.dart
@@ -0,0 +1,7 @@
+main(){
+  var adicao  = (int a , int b) => a + b;
+  print(adicao(4, 100));
+
+  var subtracao = (int a, int b) => a - b;
+  print(subtracao(4, 100));
+}
