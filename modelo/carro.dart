ee037c375fb90948ffddd7c8c8b480258edc95a6
Danilo Sacramento <89158678+DaniloSacramento@users.noreply.github.com>
2022-09-19 16:19:50 -0300

    get e set

--- modelo/carro.dart
+++ modelo/carro.dart
@@ -1,37 +1,43 @@
-class Carro{
+class Carro {
+  final int velocidadeMaxima;
+  int _velocidadeAtual = 0;
 
- final int velocidadeMaxima;
- int velocidadeAtual = 0;
+  Carro([this.velocidadeMaxima = 200]);
 
+  int get velocidadeAtual{
+    return this._velocidadeAtual;
+  }
 
+  void set velocidadeAtual(int novaVelocidade){
+    bool deltaValido = (_velocidadeAtual - novaVelocidade).abs() <= 5;
+    if(deltaValido && novaVelocidade >= 0){
+    this._velocidadeAtual = novaVelocidade;
+    }    
+  }
 
-  Carro([ this.velocidadeMaxima = 200]);
-
-  int acelerar(){
-    if(velocidadeAtual + 5 >= velocidadeMaxima){
-      velocidadeAtual = velocidadeMaxima;
-    }else{
-       velocidadeAtual += 5;
+  int acelerar() {
+    if (_velocidadeAtual + 5 >= velocidadeMaxima) {
+       _velocidadeAtual = velocidadeMaxima;
+    } else {
+       _velocidadeAtual += 5;
     }
-     return velocidadeAtual;
+    return  _velocidadeAtual;
   }
 
- int frear(){
-  if(velocidadeAtual -5 <= 0){
-    velocidadeAtual = 0;
-  }else{
-    velocidadeAtual -= 5;
-  }
-return velocidadeAtual;
-  }
-
-  bool estaNoLimite(){
-    return velocidadeAtual == velocidadeMaxima;
+  int frear() {
+    if ( _velocidadeAtual - 5 <= 0) {
+       _velocidadeAtual = 0;
+    } else {
+       _velocidadeAtual -= 5;
+    }
+    return  _velocidadeAtual;
   }
 
-   bool estaParado(){
-    return velocidadeAtual == 0;
+  bool estaNoLimite() {
+    return  _velocidadeAtual == velocidadeMaxima;
+  }
+
+  bool estaParado() {
+    return  _velocidadeAtual == 0;
   }
 }
-
-
--- teste/testecarro.dart
+++ teste/testecarro.dart
@@ -1,16 +1,20 @@
 import '../modelo/carro.dart';
 
-  main(){
-    var c1 = new Carro(320); 
+main() {
+  var c1 = new Carro(320);
 
-    while(! c1.estaNoLimite()){   // ENQUANTO O C1 NÃO ESTA NO LIMITE
-      print(" a velocidade atual é ${c1.acelerar()}");
-    }
-    
-
-    print("O carro chegou no máximo com velodidade de ${c1.velocidadeAtual}");
-
-    while(!c1.estaParado()){
-      print("O carro está freando ${c1.frear()}");
-    }
+  while (!c1.estaNoLimite()) {
+    // ENQUANTO O C1 NÃO ESTA NO LIMITE
+    print(" a velocidade atual é ${c1.acelerar()}");
   }
+
+  print("O carro chegou no máximo com velodidade de ${c1.velocidadeAtual}");
+
+  while (!c1.estaParado()) {
+    print("O carro está freando ${c1.frear()}");
+  }
+  c1.velocidadeAtual = 3;
+  c1.velocidadeAtual = 4;
+  c1.velocidadeAtual = 47; 
+  print("O carro parou com velocidade de ${c1.velocidadeAtual}");
+}
